1c7ea54aa164f06e929a2b6bd0bb93fc8d6767b1
BenjaminOffor <benjaminoffor10@gmail.com>
2025-03-15 06:47:36 +0000

    testing on Sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        //Ask the user for their name.
+        Console.WriteLine("What is your first name.");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is you last name");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {first} {last}"); 
     }
 }
+
+
+
