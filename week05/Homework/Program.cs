661669cec2b14f0619ef4e75e33de943a518be3e
BenjaminOffor <benjaminoffor10@gmail.com>
2025-04-08 18:34:22 +0000

    Updated Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,50 +1,9 @@
 using System;
 
-namespace MindfulnessProgram
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            bool exit = false;
-
-            while (!exit)
-            {
-                Console.Clear();
-                Console.WriteLine("Mindfulness Program");
-                Console.WriteLine("Menu Options:");
-                Console.WriteLine("1. Start Breathing Activity");
-                Console.WriteLine("2. Start Reflection Activity");
-                Console.WriteLine("3. Start Listing Activity");
-                Console.WriteLine("4. Quit");
-                Console.Write("Select a choice from the menu: ");
-
-                string choice = Console.ReadLine();
-
-                switch (choice)
-                {
-                    case "1":
-                        BreathingActivity breathing = new BreathingActivity();
-                        breathing.StartActivity();
-                        break;
-                    case "2":
-                        ReflectionActivity reflection = new ReflectionActivity();
-                        reflection.StartActivity();
-                        break;
-                    case "3":
-                        ListingActivity listing = new ListingActivity();
-                        listing.StartActivity();
-                        break;
-                    case "4":
-                        exit = true;
-                        Console.WriteLine("Goodbye!");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        Thread.Sleep(2000);
-                        break;
-                }
-            }
-        }
+        Console.WriteLine("Hello World! This is the Homework Project.");
     }
 }
